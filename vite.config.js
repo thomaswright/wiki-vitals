5db56142d548d9b0b46be49012b87cf5fe0c37b2
Thomas Wright <thomaswright1@gmail.com>
2026-01-27 16:00:42 -0800

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/wiki-vitals",
+
   plugins: [
     tailwindcss(),
     react({
